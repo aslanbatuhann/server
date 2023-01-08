650c400fb196d619f2b13a0e555962cb8b5cebbd
Batuhan Aslan <batuhanaslan9601@gmail.com>
2023-01-08 10:38:02 +0300

    Initial commit

---
+++ src/main/java/com/batuhanaslan/server/enumeration/Status.java
@@ -0,0 +1,18 @@
+package com.batuhanaslan.server.enumeration;
+
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+
+    SERVER_DOWN("SERVER_DOWN");
+
+    private final String status;
+
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+}
---
+++ src/main/java/com/batuhanaslan/server/model/Server.java
@@ -0,0 +1,29 @@
+package com.batuhanaslan.server.model;
+
+import com.batuhanaslan.server.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
