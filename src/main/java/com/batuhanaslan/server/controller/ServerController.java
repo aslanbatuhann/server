969e77a81c9935322ddd7938991be7d75ea84447
Batuhan Aslan <batuhanaslan9601@gmail.com>
2023-01-15 09:28:51 +0300

    http resource - pingServer

--- src/main/java/com/batuhanaslan/server/controller/ServerController.java
+++ src/main/java/com/batuhanaslan/server/controller/ServerController.java
@@ -1,13 +1,19 @@
 package com.batuhanaslan.server.controller;
 
+import com.batuhanaslan.server.enumeration.Status;
 import com.batuhanaslan.server.model.Response;
+import com.batuhanaslan.server.model.Server;
 import com.batuhanaslan.server.service.implementation.ServerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
+import static com.batuhanaslan.server.enumeration.Status.SERVER_UP;
 import static java.time.LocalDateTime.now;
 import static java.util.Map.*;
 import static org.springframework.http.HttpStatus.OK;
@@ -26,6 +32,21 @@ public class ServerController {
                         .timeStamp(now())
                         .data(of("servers", serverService.list(30)))
                         .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("servers", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping Success" : "Ping failed")
                         .status(OK)
                         .statusCode(OK.value())
                         .build()
