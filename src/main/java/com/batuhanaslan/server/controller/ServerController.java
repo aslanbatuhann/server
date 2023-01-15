f94cc4def23fa1ea414798a780d1adc71412136a
Batuhan Aslan <batuhanaslan9601@gmail.com>
2023-01-15 09:21:52 +0300

    http resource - getservers

---
+++ src/main/java/com/batuhanaslan/server/controller/ServerController.java
@@ -0,0 +1,34 @@
+package com.batuhanaslan.server.controller;
+
+import com.batuhanaslan.server.model.Response;
+import com.batuhanaslan.server.service.implementation.ServerServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Map.*;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerController {
+
+    private final ServerServiceImpl serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
